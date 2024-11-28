952ea01a61e73aa6f4c2a0c0b2c6eb506edd13af
rasulkarimov <rasul.karimov@gmail.com>
2024-11-28 17:09:26 +0200

    03 remove dublicates from sorted array

---
+++ top_150/02_remove-element.py
@@ -0,0 +1,52 @@
+"""
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+Constraints:
+
+0 <= nums.length <= 100
+0 <= nums[i] <= 50
+0 <= val <= 100
+"""
+
+class Solution(object):
+    def removeElement(self, nums, val):
+      start, end = 0, len(nums) - 1
+      while start <= end:
+          if nums[start] == val:
+              nums[start], nums[end], end = nums[end], nums[start], end - 1
+          else:
+              start +=1
+      return nums[:start]
+
+if __name__ == "__main__":
+    solution = Solution()
+    
+    nums = [3,2,2,3]
+    val = 3
+    result = solution.removeElement(nums, val)
+    print(result)
+
+    nums = [0,1,2,2,3,0,4,2]
+    val = 2
+    result = solution.removeElement(nums, val)
+    print(result)
